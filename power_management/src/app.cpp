f6611d39665ee1cb977b27a85cb8d7b3b9b3d8b5
aiba-gento <8gn24gn25@gmail.com>
2026-04-28 22:57:11 +0900

    feat:メイン処理関数追加

---
+++ power_management/src/app.cpp
@@ -0,0 +1,13 @@
+#include "power_management/app.hpp"
+
+void setup() {}
+
+void loop() {}
+
+extern "C" {
+// C言語側の関数のオーバーライド
+/**
+ * @brief Receive callback for FDCAN FIFO0.
+ */
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo0ITs) {}
+}
